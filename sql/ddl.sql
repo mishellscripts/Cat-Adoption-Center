ca3dadfe475cc8092d71526914e135284fd2917a
Michelle Song <michelle.song@sjsu.edu>
2017-11-04 14:05:09 -0700

    Initial project setup - added more to gitignore, DDL, and java file

---
+++ sql/ddl.sql
@@ -0,0 +1,68 @@
+DROP DATABASE IF EXISTS catadoptiondb;
+CREATE DATABASE catadoptiondb;
+    USE catadoptiondb;
+    CREATE TABLE adoption_center (
+        locID INT AUTO_INCREMENT PRIMARY KEY,
+        location VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE cat (
+    cID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    age INT NOT NULL,
+    gender CHAR(1) NOT NULL,
+    breed VARCHAR(20) NOT NULL,
+    adoption_fee DOUBLE,
+    locID INT,
+FOREIGN KEY (locID) REFERENCES adoption_center(locID) ON DELETE CASCADE
+);
+
+CREATE TABLE person (
+    pID INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    age INT NOT NULL,
+    experience VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE medical (
+    cID INT PRIMARY KEY,
+    disease VARCHAR(20) NOT NULL,
+    medical_fee DOUBLE,
+    FOREIGN KEY(cID) REFERENCES cat(cID) ON DELETE CASCADE
+);
+
+CREATE TABLE adoption (
+    aID INT AUTO_INCREMENT PRIMARY KEY,
+    pID INT,
+    cID INT,
+    adoption_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (pID) REFERENCES person(pID) ON DELETE CASCADE,
+    FOREIGN KEY (cID) REFERENCES cat(cID) ON DELETE CASCADE
+);
+
+delimiter //
+CREATE TRIGGER free_cat
+BEFORE INSERT ON cat
+FOR EACH ROW
+BEGIN
+    IF NEW.adoption_fee IS NULL THEN
+        INSERT INTO cat(cID, name, age, gender, breed, adoption_fee, locID)
+        VALUES(NEW.cID, NEW.name, NEW.age, NEW.gender, NEW.breed, 0, NEW.locID);
+    END IF;
+END;
+//
+delimiter ;
+
+delimiter //
+CREATE TRIGGER adoption_age_restriction
+AFTER INSERT ON adoption
+FOR EACH ROW
+BEGIN
+    IF NEW.pID IN (SELECT pID FROM person WHERE age < 12) THEN
+        DELETE FROM adoption WHERE pID=NEW.pID AND cID=NEW.cID;
+    END IF;
+END;
+//
+delimiter ;
---
+++ src/CatAdoptionCenter.java
@@ -0,0 +1,5 @@
+public class CatAdoptionCenter {
+    public static void main(String[] args) {
+        System.out.println("Running!");
+    }
+}
