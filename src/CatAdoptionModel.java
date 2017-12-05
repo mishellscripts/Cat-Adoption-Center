2c0c457332564770a47e5d629851dfb22349a087
Michelle Song <michelle.song@sjsu.edu>
2017-12-05 14:28:31 -0800

    Update model, ddl, procedures to sync up with changes, fix issue with small frame showing up with CatDirectoryFrame

--- src/CatAdoptionModel.java
+++ src/CatAdoptionModel.java
@@ -10,6 +10,13 @@ import java.util.HashMap;
 
 import javax.sql.DataSource;
 
+/**
+ * CatAdoptionModel is responsible for establishing connection to the MySQL database
+ * and contains location information specific to an adoption center and
+ * all the functions to query and update the database.
+ * @author CS157A Team CATS
+ *
+ */
 public class CatAdoptionModel {
 	private Connection connection;
 	private String location;
@@ -31,58 +38,71 @@ public class CatAdoptionModel {
 		}
 	}
 	
+	/**
+	 * Setter method to update the location of the model
+	 * @param 	id 		the location id
+	 * @param 	loc 	the location name
+	 */
 	public void setLocation(int id, String loc) {
-		//System.out.println("Setting location to " + id);
 		location = loc;
 		locID = id;
-		//System.out.println(getLocation());
 	}
 	
+	/**
+	 * Getter method to obtain the location id of the model
+	 * @return 	int 	the location id
+	 */	
 	public int getLocationID() {
 		return locID;
 	}
 	
+	/**
+	 * Getter method to obtain the location name of the model
+	 * @return 	String	the location name
+	 */
 	public String getLocationName() {
 		return location;
 	}
 	
 	/**
 	 * #1 PEOPLE can adopt a CAT from a ADOPTION_CENTER.
-	 * @param pID
-	 * @param cID
-	 * @throws SQLException 
+	 * @param 	firstName 	the first name of the user adopting the cat
+	 * @param 	lastName 	the last name of the user adopting the cat
+	 * @param 	age 		the age of the user adopting the cat
+	 * @param 	experience  the experience level of the user adopting the cat
+	 * 
 	 */
 	public void adoptCat(String firstName, String lastName, int age, String experience, int cID) throws SQLException {
-		// Add person to the db
+		
+		// Add user information to the db table Person
 		Statement st = (Statement) connection.createStatement();
 		String query = "INSERT INTO person(first_name, last_name, age, experience) VALUES "
 				+ "('" + firstName + "', '" + lastName + "', '" + age + "', '" + experience + "')";
-		
 		st.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
 		
+		// Get the id of the newly added person
 		ResultSet rs = st.getGeneratedKeys();
 		if (rs.next()) {
 		    int pID = rs.getInt(1);
-			System.out.println("ID inserted: " + pID);
 			
-			// Add adoption record to the db
+			// Add adoption record to the db table Cat
 			CallableStatement cs = connection.prepareCall("{CALL adopt_cat(?,?)}");
 			cs.setInt(1, pID);
 			cs.setInt(2, cID);
 			cs.execute();
 		} else {
-			System.out.println("Encountered an error with adding user to the database.");
+			System.out.println("Encountered an error with adding user to the database. Cannot proceed with adoption process.");
 		}
 	}
 	
 	/**
-	 * PEOPLE can donate their CAT to a ADOPTION_CENTER.
-	 * @param name
-	 * @param age
-	 * @param gender
-	 * @param breed
-	 * @param locID
-	 * @throws SQLException 
+	 * #2 PEOPLE can donate their CAT to a ADOPTION_CENTER.
+	 * @param 	name	the name of the cat being donated
+	 * @param 	age		the age of the cat being donated
+	 * @param 	gender	the gender of the cat being donated (M or F)
+	 * @param 	breed	the breed of the cat being donated
+	 * @param 	locID	the location of the adoption center the cat is donated to
+	 * 
 	 */
 	public void donateCat(String name, int age, String gender, String breed, int locID) throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL donate_cat(?,?,?,?,?)}");
@@ -96,8 +116,9 @@ public class CatAdoptionModel {
 	
 	/**
 	 * #3 PEOPLE can search for a ADOPTION_CENTER.
-	 * STORED PROCEDURE
-	 * @param location the location entered by user
+	 * If no adoption center is found in location, the result location ID is 0.
+	 * @param 	location 	the location entered by user
+	 * @return	int			the location id corresponding to the location searched
 	 * 
 	 */
 	public int searchAdoptionCenter(String location) throws SQLException {
@@ -108,15 +129,13 @@ public class CatAdoptionModel {
 		
 		if (rs.next()) {
 			locID = rs.getInt("locID"); 
-			//String loc = rs.getString("location"); 
-			//System.out.println("locID:" + locID + " Location:" + loc);
 		}
 		return locID;
 	}
 
 	/**
 	 * #4 PEOPLE can search for a CAT from a ADOPTION_CENTER they are currently visiting.
-	 * Stored procedure
+	 * @return	data containing all the cat tuples in a 2D ArrayList
 	 */
 	public ArrayList<ArrayList<String>> searchAdoptionCenterCats() throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL search_adoption_center_cats(?)}");
@@ -162,7 +181,7 @@ public class CatAdoptionModel {
 
 	/**
 	 * #5 PEOPLE can view all CAT MEDICAL records from the ADOPTION_CENTER they are currently visiting
-	 * STORED PROCEDURE
+	 * @return	data containing all the cat medical records from current location tuples in a 2D ArrayList
 	 */
 	public ArrayList<ArrayList<String>> searchAdoptionCenterRecords() throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL search_adoption_center_records(?)}");
@@ -201,8 +220,7 @@ public class CatAdoptionModel {
 
 	/**
 	 * #6 PEOPLE can return the CAT to the ADOPTION_CENTER they adopted from.
-	 * STORED PROCEDURE
-	 * @param id the cat id
+	 * @param 	id 		the id of the cat being returned to the adoption center
 	 */
 	public void returnCat(int id) throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL return_cat(?)}");
@@ -211,10 +229,8 @@ public class CatAdoptionModel {
 	}
 
 	/**
-	 * #7 ADOPTION_CENTER can view all CAT�s and their MEDICAL record
-	 * STORED PROCEDURE
-	 * TODO: procedure has been altered as of 11/15
-	 * @throws SQLException
+	 * #7 ADOPTION_CENTER can view all CATs and their MEDICAL records
+	 * @return	data containing all the cat medical record tuples in a 2D ArrayList
 	 */
     public ArrayList<ArrayList<String>> searchCats() throws SQLException {
         Statement cs = (Statement) connection.createStatement();
@@ -282,13 +298,13 @@ public class CatAdoptionModel {
 
 	/**
 	 * #9 ADOPTION_CENTER can register a found CAT.
-	 * STORED PROCEDURE
-	 * @param name
-	 * @param age
-	 * @param gender
-	 * @param fee
-	 * @param locID
-	 * @throws SQLException
+	 * @param 	name	the name of the cat being registered
+	 * @param 	age		the age of the cat being registered
+	 * @param 	gender	the gender of the cat being registered (M or F)
+	 * @param 	breed	the breed of the cat being registered
+	 * @param	fee		the price of the cat being registered (base fee)
+	 * @param 	locID	the location of the adoption center the cat is donated to
+	 * 
 	 */
 	public void insertCat(String name, int age, String gender, String breed, double fee, int locID) throws SQLException {
 		// referencing slide 32 of JDBC
@@ -306,57 +322,55 @@ public class CatAdoptionModel {
 	}
 
 	/**
-	 * TODO: determine whether #10 need methods or are purely for triggers
-	 */
-
-	/**
-	 * #11 ADOPTION_CENTER can remove a CAT's MEDICAL record. 
-	 * @param cID cat ID
+	 * #10 ADOPTION_CENTER can remove a CAT's MEDICAL record. 
+	 * @param 	cID			the id of the cat to remove
+	 * @param	disease		the disease name to remove
+	 * 
 	 */
 	public void removeMedical(int cID, String disease) throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL remove_cat_medical_record(?, ?)}");
 		cs.setInt(1, cID);
 		cs.setString(2, disease);
-		int test = cs.executeUpdate();
+		int test = cs.executeUpdate(); //TODO: take out the int test during finalization
 		System.out.println("Affected rows removeMedical: " + test);
 	}
 
 
 	/**
-	 * #12 ADOPTION_CENTER can register a CAT's MEDICAL record.
-	 * @param cID
-	 * @param disease
-	 * @param fee
-	 * @throws SQLException
+	 * #11 ADOPTION_CENTER can register a CAT's MEDICAL record.
+	 * @param 	cID			the id of the cat to add record to
+	 * @param 	disease		the disease name to add 
+	 * @param 	fee			the medical fee amount for the disease
+	 * 
 	 */
 	public void addMedical(int cID, String disease, double fee) throws SQLException {
 		CallableStatement cs = connection.prepareCall("{CALL register_cat_medical_record(?,?,?)}");
 		cs.setInt(1, cID);
 		cs.setString(2, disease);
 		cs.setDouble(3, fee);
-		int test = cs.executeUpdate();
+		int test = cs.executeUpdate(); //TODO: take out the int test during finalization
 		System.out.println("Affected rows addMedical: " + test);
 	}
 
 	/**
-	 * #13 ADOPTION_CENTER can update a CAT's MEDICAL record.
-	 * @param cID
-	 * @param disease
-	 * @param fee
-	 * @throws SQLException
+	 * #12 ADOPTION_CENTER can update a CAT's MEDICAL record.
+	 * @param 	cID			the id of the cat whose record will be updated
+	 * @param 	disease		the disease of the cat that will be updated
+	 * @param 	fee			the new fee amount to update to
+	 * 
 	 */
 	public void updateMedical(int cID, String disease, double fee)  throws SQLException  {
 		CallableStatement cs = connection.prepareCall("{CALL update_cat_medical_fee(?,?,?)}");
 		cs.setInt(1,cID);
 		cs.setString(2, disease);
 		cs.setDouble(3, fee);
-		int test = cs.executeUpdate();
+		int test = cs.executeUpdate(); //TODO: take out the int test during finalization
 		System.out.println("Affected rows updateMedical: " + test);
 	}
 
 	/**
-	 * #14 ADOPTION_CENTER can view all ADOPTION records.
-	 * @throws SQLException
+	 * #13 ADOPTION_CENTER can view all ADOPTION records.
+	 * @return	data containing all the adoption records tuples in a 2D ArrayList
 	 */
 	public ArrayList<ArrayList<String>> viewAllAdoptions() throws SQLException {		
 		Statement cs = (Statement) connection.createStatement();
@@ -390,14 +404,17 @@ public class CatAdoptionModel {
 	}
 
 	/**
-	 * #15 ADOPTION_CENTER can determine if the person (PEOPLE) is qualified to adopt
+	 * #14 ADOPTION_CENTER can determine if the person (PEOPLE) is qualified to adopt
 	 * a specific CAT based on person experience level.
-	 * @param experience
-	 * @return whether there are rows in the result set
-	 * @throws SQLException
+	 * @param 	experience	the experience level of the person
+	 * @param 	cID			the id of the cat in question
+	 * @return 	boolean		the boolean result of whether the person is qualified or not	
+	 *
 	 */
 	public boolean isPersonQualified(String experience, int cID) throws SQLException {
 		String query = "";
+		
+		// Build the query depending on person's experience
 		if (experience.equalsIgnoreCase("low")) {
 			query = "Select cID, cName, age From cat c Where (age > 3) "
 					+ "And cID = " + cID
@@ -412,9 +429,6 @@ public class CatAdoptionModel {
 			query = "Select cID, cName, age from cat";
 		}
 		
-		/*PreparedStatement ps = connection.prepareStatement(query);
-		ps.setInt(1, cID);
-		ResultSet rs = ps.executeQuery(query);*/
 		Statement cs = (Statement) connection.createStatement();
 		ResultSet rs = cs.executeQuery(query);
 		
@@ -422,17 +436,16 @@ public class CatAdoptionModel {
 	}
 
 	/**
-	 * #16 ADOPTION_CENTER can determine if the CAT is adoptable based
+	 * #15 ADOPTION_CENTER can determine if the CAT is adoptable based
 	 * on the CAT's medical record (MEDICAL).
-	 * @return if there are rows in this result set
-	 * @throws SQLException
+	 * @return 	boolean		result indicating whether a cat is healthy enough to be adopted
+	 * 
 	 */
-	public boolean isCatQualified(int cID) throws SQLException {
-		String query = "Select cID from cat c Where cID = " + cID +
-				" and cID not in (select cID from medical where disease = 'Rabies') " +
-				"and cID not in (select cID from medical where disease = 'Ringworm')";
-		/*PreparedStatement ps = connection.prepareStatement(query);
-		ps.setInt(1, cID);*/
+	public boolean isCatQualified(int cID) throws SQLException {		
+		String query = "SELECT * FROM cat c WHERE c.cID = " + cID + 
+				" AND c.cID NOT IN (SELECT cID FROM medical m WHERE cID=c.cID AND disease='Rabies'" +       
+				" UNION SELECT cID FROM medical m WHERE cID=c.cID AND disease='Ringworm')";
+
 		Statement st = (Statement) connection.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		
@@ -440,9 +453,10 @@ public class CatAdoptionModel {
 	}
 	
 	/**
-	 * ADOPTION_CENTER can calculate the cost of a CAT based on its adoption fee and medical fee. 
-	 * @return sum of adoption and medical
-	 * @throws SQLException
+	 * #16 ADOPTION_CENTER can calculate the cost of a CAT based on its adoption fee and medical fee. 
+	 * @param	cID		the id of the cat whose total fee will be calculated
+	 * @return 	double	sum of adoption and medical
+	 * 
 	 */
 	public double calculateCosts(int cID) throws SQLException {
 		Statement st = connection.createStatement();
